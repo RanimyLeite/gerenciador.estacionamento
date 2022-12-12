7e4c9b2114bca3ba4e6146836b879427fbe01172
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-12-12 11:11:50 -0300

    test - criação de projeto inicial para testes

---
+++ gerenciador.estacionamento.Testes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace gerenciador.estacionamento.Testes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
